8258353f7f94a63de5a39f1818cb7247d946724f
MahajanGaurav5 <mahajangaurav547@gmail.com>
2023-09-05 17:41:42 +0530

    TO-do-List with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To-do-List';
+  title = 'To-do-List with Angular';
+  list:any[]=[];
+  isTextCentered: boolean = true;
+
+  addTask(value: string) {
+    if (value) {
+      this.list.push({ name: value, id: this.list.length + 1 });
+      this.isTextCentered = false;
+    }
+  }
+  
+
+  // addTask(item:String)
+  // {
+  //   this.list.push({id:this.list.length,name:item})
+  //   console.warn(this.list);
+  // }
+
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(item=>item.id!==id)
+
+  }
 }
